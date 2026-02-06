1cedac19490f338c08bffb93a0a41c87041b2a9a
Romain Morvant <romainmorvantdev@gmail.com>
2026-02-06 11:15:12 +0100

    Y axis added in generate for loop

--- scripts/world.js
+++ scripts/world.js
@@ -4,17 +4,19 @@ const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshLambertMaterial({ color: 0x00d000 });
 
 export class World extends THREE.Group {
-  constructor(size = 32) {
+  constructor(size = { width: 32, height: 16 }) {
     super();
     this.size = size;
   }
 
   generate() {
-    for (let x = 0; x < this.size; x++) {
-      for (let z = 0; z < this.size; z++) {
-        const block = new THREE.Mesh(geometry, material);
-        block.position.set(x, 0, z);
-        this.add(block);
+    for (let x = 0; x < this.size.width; x++) {
+      for (let y = 0; y < this.size.height; y++) {
+        for (let z = 0; z < this.size.width; z++) {
+          const block = new THREE.Mesh(geometry, material);
+          block.position.set(x, y, z);
+          this.add(block);
+        }
       }
     }
   }
