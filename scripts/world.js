8511c676cec3da5e3b3504943ec4ddcd1c250879
Romain Morvant <romainmorvantdev@gmail.com>
2026-02-06 12:20:10 +0100

    Ajout des helperFunctions: getBlock, setBlockId, setBlockInstanceId & inBounds

--- scripts/world.js
+++ scripts/world.js
@@ -4,12 +4,52 @@ const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshLambertMaterial({ color: 0x00d000 });
 
 export class World extends THREE.Group {
+  /**
+   * @type {{
+   * id: number,
+   * instanceId: number
+   * }[][][]}
+   */
+  data = [];
+
   constructor(size = { width: 64, height: 32 }) {
     super();
     this.size = size;
   }
 
+  /**
+   * Génère les données et les maillages (meshes) du monde
+   */
   generate() {
+    this.generateTerrain();
+    this.generateMeshes();
+  }
+
+  /**
+   * Génère les données du terrain du monde
+   */
+  generateTerrain() {
+    this.data = [];
+    for (let x = 0; x < this.size.width; x++) {
+      const slice = [];
+      for (let y = 0; y < this.size.height; y++) {
+        const row = [];
+        for (let z = 0; z < this.size.width; z++) {
+          row.push({
+            id: 1,
+            instanceId: null,
+          });
+        }
+        slice.push(row);
+      }
+      this.data.push(slice);
+    }
+  }
+
+  /**
+   * Génère la représentation 3D du monde à partir des données de ce monde
+   */
+  generateMeshes() {
     this.clear();
     const maxCount = this.size.width * this.size.width * this.size.height;
     const mesh = new THREE.InstancedMesh(geometry, material, maxCount);
@@ -27,5 +67,68 @@ export class World extends THREE.Group {
     }
 
     this.add(mesh);
+  }
+
+  /**
+   * Récupère les données du bloc à (x, y, z)
+   * @param {number} x
+   * @param {number} y
+   * @param {number} z
+   * @returns {{id: number, instanceId: number}}
+   */
+  getBlock(x, y, z) {
+    if (this.inBounds(x, y, z)) {
+      return this.data[x][y][z];
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Définit l'id du bloc pour le bloc situé à (x, y, z)
+   * @param {number} x
+   * @param {number} y
+   * @param {number} z
+   * @param {number} id
+   */
+  setBlockId(x, y, z, id) {
+    if (this.inBounds(x, y, z)) {
+      this.data[x][y][z].id = id;
+    }
+  }
+
+  /**
+   * Définit l'identifiant d'instance du bloc pour le bloc situé à (x, y, z)
+   * @param {number} x
+   * @param {number} y
+   * @param {number} z
+   * @param {number} instanceId
+   */
+  setBlockInstanceId(x, y, z, instanceId) {
+    if (this.inBounds(x, y, z)) {
+      this.data[x][y][z].instanceId = instanceId;
+    }
+  }
+
+  /**
+   * Vérifie si les coordonnées (x, y, z) sont comprises dans les limites.
+   * @param {number} x
+   * @param {number} y
+   * @param {number} z
+   * @returns {boolean}
+   */
+  inBounds(x, y, z) {
+    if (
+      x >= 0 &&
+      x < this.size.width &&
+      y >= 0 &&
+      y < this.size.height &&
+      z >= 0 &&
+      z < this.size.width
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
