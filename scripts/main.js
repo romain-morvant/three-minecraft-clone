4c4c02539a3691a071a4223b0a03999cf172dd46
Romain Morvant <romainmorvantdev@gmail.com>
2026-02-06 11:07:39 +0100

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,54 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+// Mise en place du renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x80a0e0);
+document.body.appendChild(renderer.domElement);
+
+// Mise en place de la caméra
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+);
+camera.position.set(-32, 16, -32);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(16, 0, 16);
+controls.update();
+
+// Création de la scène
+const scene = new THREE.Scene();
+
+// Éclairage
+function setupLights() {
+  const light1 = new THREE.DirectionalLight();
+  light1.position.set(1, 1, 1);
+  scene.add(light1);
+
+  const light2 = new THREE.DirectionalLight();
+  light2.position.set(-1, 1, -0.5);
+  scene.add(light2);
+
+  const ambient = new THREE.AmbientLight();
+  ambient.intensity = 0.1;
+  scene.add(ambient);
+}
+
+// Render loop
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+setupLights();
+setupWorld(32);
+animate();
